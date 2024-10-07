2ef6524cfed3c433ebebd3bbfd771bad3f37d3f0
Robert Horosewski <horoserp@yahoo.com>
2024-10-07 16:10:29 -0400

    Update: Add FontAwesome icon to Navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+// Steps to use Fontawsome Icons:
+// 1. Add SVG Core (npm i --save @fortawesome/fontawesome-svg-core)
+// 2. Add icon Packages
+// npm i --save @fortawesome/free-solid-svg-icons
+// npm i --save @fortawesome/free-regular-svg-icons
+// npm i --save @fortawesome/free-brands-svg-icons
+// 3. Add the React Component
+// npm i --save @fortawesome/react-fontawesome@latest
+// 4. Import the Fontawesome Component
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// 5. Import the specific Fontawesome icon from correct library
+import { faTypo3 } from "@fortawesome/free-brands-svg-icons";
 
 function Navbar() {
   return (
@@ -7,7 +19,8 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            TRVL <i className="fab fa-typo3" />
+            {/* Use FontAwesomeIcon component with imported icon */}
+            TRVL <FontAwesomeIcon icon={faTypo3} />
           </Link>
         </div>
       </nav>
