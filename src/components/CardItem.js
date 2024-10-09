9b311868cf95b35c3b34e7a38b1ec7166b46216b
Robert Horosewski <horoserp@yahoo.com>
2024-10-09 15:57:02 -0400

    Fix: Correct classNames on Link and img.

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,9 +5,9 @@ function CardItem(props) {
   return (
     <>
       <li className="cards__item">
-        <Link className="card__item__link" to={props.path}>
+        <Link className="cards__item__link" to={props.path}>
           <figure className="cards__item__pic-wrap" data-category={props.label}>
-            <img src={props.src} alt="Travel" className="cards__item__image" />
+            <img src={props.src} alt="Travel" className="cards__item__img" />
           </figure>
           <div className="cards__item__info">
             <h5 className="cards__item__text">{props.text}</h5>
