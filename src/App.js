334bf99b9f4da7c1c48245d9fe3766bbf847b163
Robert Horosewski <horoserp@yahoo.com>
2024-10-12 19:21:17 -0400

    Update: Finish pages. Finish footer.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import Navbar from "./components/Navbar.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.js";
+import Services from "./components/pages/Services.js";
+import Products from "./components/pages/Products.js";
+import SignUp from "./components/pages/SignUp.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
@@ -10,7 +14,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
